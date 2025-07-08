1433e38c3cbae1ed157ccf3d94a09263b85b236a
Laura-Villain <laura.villain@esqlabs.com>
2025-07-08 12:42:39 +0200

    put back the name as it was before + deletion of report

--- Evaluation/workflow.R
+++ Evaluation/workflow.R
@@ -60,8 +60,8 @@ createQualificationReport <- function(qualificationRunnerFolder,
   #' `workingDirectory`: current directory is used as default working directory
   workingDirectory <- getwd()
   
-  qualificationPlanName <- "qualification_plan.json"
-  qualificationPlanFile <- file.path(workingDirectory, "input", qualificationPlanName)
+  qualificationPlanName <- "evaluation_plan.json"
+  qualificationPlanFile <- file.path(workingDirectory, "Input", qualificationPlanName)
   
   #' The default outputs of qualification runner should be generated under `<workingDirectory>/re_input`
   reInputFolder <- file.path(workingDirectory, "re_input")
